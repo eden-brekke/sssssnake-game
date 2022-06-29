8bfada2534980e0141fcc9df57d90b492a988247
Eden Brekke <eden.lorrai@gmail.com>
2022-06-28 18:20:18 -0700

    added colors

--- ssssnake.py
+++ ssssnake.py
@@ -4,3 +4,9 @@ import random
 
 window_x_axis = 1000
 window_y_axis = 500
+
+dark_blue = pygame.Color(0, 0, 102)
+hot_pink = pygame.Color(255, 0, 102)
+apple_green = pygame.Color(0, 255, 0)
+white = pygame.Color(255, 255, 255)
+yellow = pygame.Color(255, 255, 0)
